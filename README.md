0d1699d5bd6fc7d2c305ff6dd6619387c52de96a
tcrain96 <tristan.crain@outlook.com>
2022-01-17 17:45:34 -0500

    added basic functionality that enables user input and creation of README

---
+++ README.md
@@ -0,0 +1,35 @@
+
+  
+  # Test-Project
+  
+  ## Description
+  This is a test project
+  
+  ## Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questions](#questions)
+    
+  ## Installation
+  To install, simply download the full directory and install inquirer on your machine
+
+  ## Usage
+  To use the application, all you need to do is open up your console whilst in the directory and type in node index to start the process
+
+  ## License
+  Mozilla
+
+  ## Contributing
+  The contributors are
+
+  ## Tests
+  Simply follow the prompts and fill in the information as asked. Once complete, open the README file that is created and you will find within your information formated properly
+
+  ## Questions
+  Tristan
+  tcrain96
+
+  
--- index.js
+++ index.js
@@ -1,13 +1,138 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const generateMarkup = require("./utils/generateMarkup");
+const writeFile = require("./utils/generateReadme");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is your name? (Required)",
+    "What is your GitHub username? (Required)",
+    "What is the name of your project? (Required)",
+    "Please provide a short description of you application. (Required)",
+    "Please explain how to install your application (Required)",
+    "Please give a brief explanation of your applications usage (Required)",
+    "Please select the liscence associated with your application. (Required)",
+    "Please tell me who else contributed to this project",
+    "Please explain how to test your application. (Required)",
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const promptReadme = readmeData =>{
+    return inquirer.prompt([
+        {
+            type:"input",
+            name: "userName",
+            message: questions[0],
+            validate:userName =>{
+                if(userName){
+                    return true;
+                }
+                else{
+                    console.log("You need to enter a name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type:"input",
+            name: "github",
+            message: questions[1],
+            validate:github =>{
+                if(github){
+                    return true;
+                }
+                else{
+                    console.log("You need to enter a GitHub username!");
+                    return false;
+                }
+            }
+        },
+        {
+            type:"input",
+            name: "projectName",
+            message: questions[2],
+            validate:projectName =>{
+                if(projectName){
+                    return true;
+                }
+                else{
+                    console.log("You need to enter a project name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type:"input",
+            name: "description",
+            message: questions[3],
+            validate:description =>{
+                if(description){
+                    return true;
+                }
+                else{
+                    console.log("You need to enter a description for your project!");
+                    return false;
+                }
+            }
+        },
+        {
+            type:"input",
+            name: "installInstructions",
+            message: questions[4],
+            validate:installInstructions =>{
+                if(installInstructions){
+                    return true;
+                }
+                else{
+                    console.log("You need to explain how to install your application!");
+                    return false;
+                }
+            }
+        },
+        {
+            type:"input",
+            name: "usage",
+            message: questions[5],
+            validate:usage =>{
+                if(usage){
+                    return true;
+                }
+                else{
+                    console.log("You need to explain how to use your application!");
+                    return false;
+                }
+            }
+        },
+        {
+            type:"list",
+            name: "licences",
+            message: questions[6],
+            choices: ["Apache 2.0","BSD 3-Clause","BSD 2-Clause","GPL","LGPL","MIT","Mozilla","Eclipse"]
+        },
+        {
+            type:"input",
+            name: "contributing",
+            message: questions[7],
+        },
+        {
+            type:"input",
+            name: "testCases",
+            message: questions[8],
+            validate:testCases =>{
+                if(testCases){
+                    return true;
+                }
+                else{
+                    console.log("You need to explain how to test your application!");
+                    return false;
+                }
+            }
+        } 
+        
+    ]);
+};
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+promptReadme()
+    .then(readmeData => {
+        return generateMarkup(readmeData);
+    })
+    .then(readmeText =>{
+        return writeFile(readmeText);
+    });
